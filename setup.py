912efafa82b07d060a24a543f788b8ee28396000
Haxp <1196039325@qq.com>
2024-03-27 16:36:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,13 +46,13 @@ with codecs.open('README.rst', 'r', 'utf-8') as f:
 
 
 setup(
-    name='newspaper3k',
-    version='0.3.0',
+    name='hsy1_newspaper3k',
+    version='0.3.3',
     description='Simplified python article discovery & extraction.',
     long_description=readme,
-    author='Lucas Ou-Yang',
-    author_email='lucasyangpersonal@gmail.com',
-    url='https://github.com/codelucas/newspaper/',
+    author='Siyuan Huang',
+    author_email='huangsiyuan924@gmail.com',
+    url='https://github.com/huangsiyuan924/newspaper/',
     packages=packages,
     include_package_data=True,
     install_requires=required,
